019a2f2eff9e31805eb25f0c16018ed9d5a406e5
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-13 01:18:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from encryption_module import encrypt_text
+from decryption_module import decrypt_text
+
+if __name__ == "__main__":
+    message = "Welcome to my world!"
+    key = "mysecretkey"
+    cipher_text = encrypt_text(message, key)
+    print(f"Encrypted Message: {cipher_text}")
+    
+    cipher_text = "X19fX2BgbG1lbm" 
+    key = "mysecretkey"
+    decrypted_message = decrypt_text(cipher_text, key)
+    print(f"Decrypted Message: {decrypted_message}")
