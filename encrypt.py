bc79a1118e1c94ec1805d2407cd2d08287d5c736
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-13 01:17:18 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -9,10 +9,3 @@ def encrypt_text(plain_text, key):
     base64_bytes = base64.b64encode(encrypted_data.encode())
     base64_str = base64_bytes.decode('ascii')
     return base64_str
-
-if __name__ == "__main__":
-    message = "Welcome to my world!"
-    
-    key = "mysecretkey"
-    cipher_text = encrypt_text(message, key)
-    print(f"Encrypted Message: {cipher_text}")
